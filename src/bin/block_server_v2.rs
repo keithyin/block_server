542f07a7e0ea5dc60df602f815ca6c2b61d36899
keith <ke1thyin@outlook.com>
2025-11-28 09:37:11 +0000

    v2 is ok

--- src/bin/block_server_v2.rs
+++ src/bin/block_server_v2.rs
@@ -1,15 +1,14 @@
 use std::{
     io,
-    sync::{Arc, Mutex, OnceLock, atomic::AtomicBool},
+    sync::{Arc, Mutex, OnceLock, atomic::AtomicU8},
 };
 
-use anyhow::Context;
 use block_server::net::{
-    ControlInfo, ControlResponse, extract_meta_info,
-    protocol::{ClientDataReq, ClientFpReq},
+    extract_meta_info,
+    protocol::{ClientDataReq, ClientFpReq, DataMetaResp},
 };
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
@@ -17,7 +16,6 @@ use affinity;
 use clap::{self, Parser};
 use num_cpus;
 
-static READ_FLAG: AtomicBool = AtomicBool::new(true);
 static SERVED_FILES: OnceLock<Arc<Mutex<Vec<String>>>> = OnceLock::new();
 
 #[derive(Debug, Parser, Clone)]
@@ -53,9 +51,10 @@ impl Cli {
     }
 }
 
-async fn data_msg_listener() -> anyhow::Result<()> {
+async fn data_msg_listener(retry_times: Arc<AtomicU8>) -> anyhow::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:30002").await?;
     tracing::info!("data msg processor listening on 0.0.0.0:30002");
+    retry_times.store(0, std::sync::atomic::Ordering::SeqCst);
     loop {
         match listener.accept().await {
             Ok((socket, _)) => {
@@ -89,6 +88,7 @@ async fn data_msg_processor(mut socket: TcpStream) -> anyhow::Result<()> {
     socket.write_all(&file_meta_bytes).await?;
 
     let data_req_msg = extract_meta_info::<ClientDataReq>(&mut socket).await?;
+    let channel_end = data_req_msg.channel_end;
     let mut pos_data_start = data_req_msg.get_pos_data_start();
     let mut neg_data_start = data_req_msg.get_neg_data_start();
     let mut channel_start = data_req_msg.channel_start;
@@ -99,28 +99,64 @@ async fn data_msg_processor(mut socket: TcpStream) -> anyhow::Result<()> {
             } else {
                 0
             });
-    
-    
-    let mut buf: Vec<u8> = Vec::with_capacity(1024 * 1024 * 2);
+
+    let mut buf: Vec<u8> = Vec::with_capacity(buf_size);
+
     loop {
-        buf.clear();
-        let size = f.read_buf(&mut buf).await.context("read file error")?;
-        if size == 0 {
-            tracing::info!("file send done");
+        // read a batch data
+        let cur_batch_size = (channel_end - channel_start).min(data_req_msg.batch_size);
+        let cur_positive_size = cur_batch_size * data_req_msg.positive_data_per_channel_length;
+        let cur_negative_size = if data_req_msg.use_negative {
+            cur_batch_size * data_req_msg.negative_data_per_channel_length
+        } else {
+            0
+        };
+
+        f.seek(io::SeekFrom::Start(pos_data_start as u64)).await?;
+        f.read_exact(&mut buf[..cur_positive_size]).await?;
+        if data_req_msg.use_negative {
+            f.seek(io::SeekFrom::Start(neg_data_start as u64)).await?;
+            f.read_exact(&mut buf[cur_positive_size..(cur_positive_size + cur_negative_size)])
+                .await?;
+        }
+
+        let resp_meta = DataMetaResp {
+            channel_start: channel_start,
+            num_channels: cur_batch_size,
+            positive_data_length: cur_positive_size,
+            negative_data_length: cur_negative_size,
+        };
+        let resp_json = serde_json::to_string(&resp_meta)?;
+        let resp_json_bytes = resp_json.as_bytes().to_vec();
+        socket
+            .write_all(&resp_json_bytes.len().to_le_bytes())
+            .await?;
+        socket.write_all(&resp_json_bytes).await?;
+        socket
+            .write_all(&buf[..(cur_positive_size + cur_negative_size)])
+            .await?;
+
+        channel_start += cur_batch_size;
+        pos_data_start += cur_positive_size;
+        neg_data_start += cur_negative_size;
+
+        if channel_start >= channel_end {
             break;
         }
-        socket
-            .write_all(&buf[..size])
-            .await
-            .context("write socket error")?;
-
-        // receiver ack from compute server
-        let mut resp_bytes = [0_u8; 1];
-        socket
-            .read_exact(&mut resp_bytes)
-            .await
-            .context("wait response error")?;
     }
+
+    let resp_meta = DataMetaResp {
+        channel_start: channel_start,
+        num_channels: 0,
+        positive_data_length: 0,
+        negative_data_length: 0,
+    };
+    let resp_json = serde_json::to_string(&resp_meta)?;
+    let resp_json_bytes = resp_json.as_bytes().to_vec();
+    socket
+        .write_all(&resp_json_bytes.len().to_le_bytes())
+        .await?;
+    socket.write_all(&resp_json_bytes).await?;
 
     Ok(())
 }
@@ -155,7 +191,20 @@ fn main() -> io::Result<()> {
         .build()?;
 
     rt.block_on(async move {
-        data_msg_listener().await;
+        let retry_times = Arc::new(AtomicU8::new(0));
+        loop {
+            match data_msg_listener(retry_times.clone()).await {
+                Ok(_) => {}
+                Err(err) => {
+                    tracing::error!("try: {:?}, err: {}", retry_times, err);
+                    retry_times.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                }
+            }
+            if retry_times.load(std::sync::atomic::Ordering::SeqCst) > 20 {
+                tracing::error!("retry error. break now");
+                break;
+            }
+        }
     });
     Ok(())
 }
